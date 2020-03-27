bea3221712599e76acb9c69d12effbc73a9fa369
digitruck-rafiul <rafiul.islam@digitruck.com.bd>
2020-03-28 00:36:07 +0600

    Using cors and body parser in api

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({data: 'hello world'});
