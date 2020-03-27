198c1e8784fb455c78f5b3243e536b8d559c21fd
digitruck-rafiul <rafiul.islam@digitruck.com.bd>
2020-03-27 21:21:32 +0600

    Testing api server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.json('hello world');
+  res.json({data: 'hello world'});
 });
 
 app.listen(port, () => {
